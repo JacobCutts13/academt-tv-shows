95bd9d66e186d335cf9541e91cdee63f1bec2dae
Jc <Jc@email.com>
2022-03-24 20:19:12 +0000

    Less lag by only keeping first 5 elements of meme array

--- src/components/meme.tsx
+++ src/components/meme.tsx
@@ -1,9 +1,12 @@
 import { memeProps, memesProps } from "./interfaces";
 
 export default function Memes(props: memesProps): JSX.Element {
+    const length = props.memeArray.length
+    props.memeArray.splice(5, length-1) //show top 5 memes
   return (
     <div className="memes">
       <h1>Top meme for this episode</h1>
+    
       {props.memeArray.map((meme: memeProps) => (
         <div className="memeBox" key={meme.title}>
           <h2>{meme.title}</h2>
